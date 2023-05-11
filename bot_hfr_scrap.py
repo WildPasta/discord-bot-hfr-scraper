12eb6aede3c60862cea8c38ed1859547a1513155
wildpasta <chauve.richard@protonmail.com>
2023-05-11 11:54:14 +0200

    Initial commit

---
+++ bot_hfr_scrap.py
@@ -0,0 +1,102 @@
+"""
+# hfr_scraper.py v 1.1
+# Written by WildPasta
+# Purpose: web scraper to retrieve hardware available on HFR website
+"""
+
+# Imports
+import discord
+import os
+import requests
+import sys
+
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
+
+def get_ads(deep, keyword):
+    """
+    Retrieve all ads topics on HFR containing the keyword
+    Deep is the number of pages scraped
+    Keyword is the keyword to search for in the ads titles
+    """
+
+    # Empty dictionary containing the URL as key and title as value
+    ads_dict = {}
+
+    # Scrap the number of pages given in arg
+    for i in range(0, deep, 1):
+        url = f"https://forum.hardware.fr/hfr/AchatsVentes/Hardware/liste_sujet-{i}.htm"
+        response = requests.get(url)
+
+        # Parse HTML using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Retrieve the title and URL of the ads in the specified column
+        ads = soup.find_all("td", {"class": "sujetCase3"})
+
+        # Iterate through all of the ads
+        for ad in ads:
+            # Find the cCatTopic element within the current ad
+            cCatTopic = ad.find("a", {"class": "cCatTopic"})
+
+            # Check if the cCatTopic element exists and its text contains the keyword
+            if cCatTopic and keyword.lower() in cCatTopic.text.lower() and "vds" in cCatTopic.text.lower():
+                url = "https://forum.hardware.fr" + cCatTopic["href"]
+                # Check if the URL is unique in our dictionary
+                if url not in ads_dict:
+                    ads_dict[url] = cCatTopic.text
+
+    return ads_dict
+
+def main():
+    # Load the environment variables
+    load_dotenv()
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+    # Deep variable for the scraper
+    deep = 5
+
+    # Load the Discord intents
+    intents = discord.Intents.all()
+    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+
+    # Print a message when bot is ready
+    @bot.event
+    async def on_ready():
+        print(f'The Discord_Bot is now ready!')   
+    
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            em = discord.Embed(title=f"Wait a second!",description=f"Réessaye dans {error.retry_after:.2f}s.")
+            await ctx.send(embed=em, delete_after=10.0)
+
+    @bot.command(name="search")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def search(ctx, keyword):
+        ads_dict = get_ads(deep, keyword)
+        
+        # Check if no ad is found
+        if len(ads_dict) == 0:
+            await ctx.send(f"No ad found containing the keyword '{keyword}'...")
+        else:
+            embed = discord.Embed(title=f"Search results for '{keyword}':", color=discord.Color.blue())
+
+            count = 0
+            for url, title in ads_dict.items():
+                # Add the title and URL to the embed
+                embed.add_field(name=title, value=url, inline=False)
+
+                count += 1
+                if count >= 10:
+                    break
+
+            await ctx.send(embed=embed)
+
+    bot.run(DISCORD_TOKEN)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ new_alert.py
@@ -0,0 +1,88 @@
+"""
+# new_alert.py v 1.0
+# Written by WildPasta
+# Purpose: get update on HFR ads containing the keyword
+"""
+
+import discord
+import os
+import sqlite3
+import sys
+
+from dotenv import load_dotenv
+
+import bot_hfr_scrap
+
+database = "hardware.db"
+
+def sql_additem(item_type, item_name, item_url):
+    dbSocket = sqlite3.connect(database)
+    cursor = dbSocket.cursor()
+
+    req = "INSERT INTO items (item_type, item_name, item_url) VALUES (?, ?, ?)"
+    data = (item_type, item_name, item_url)
+    cursor.execute(req, data)
+    dbSocket.commit()
+    cursor.close()
+
+def is_url_in_database(url):
+    """
+    Vérifie si l'URL est déjà présente dans la base de données
+    """
+    dbSocket = sqlite3.connect(database)
+    cursor = dbSocket.cursor()
+
+    req = "SELECT COUNT(*) FROM items WHERE item_url = ?"
+    cursor.execute(req, (url,))
+    count = cursor.fetchone()[0]
+
+    cursor.close()
+
+    return count > 0
+
+def retrieve_new_ads(ads):
+    new_ads = []
+    for url, title in ads.items():
+        if not is_url_in_database(url):
+            new_ads.append((title, url))
+    return new_ads
+
+async def send_message(client, message):
+    embed = discord.Embed(
+        title="Nouvelles annonces de serveurs HFR",
+        description=message,
+        color=discord.Color.green()
+    )
+    channel = client.get_channel(1052934887100915763)
+    await channel.send(embed=embed)
+
+def main():
+    load_dotenv()
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+    
+    intents = discord.Intents.all()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        keyword = "serveur"
+        ads = bot_hfr_scrap.get_ads(10, keyword)
+        new_ads = retrieve_new_ads(ads)
+        if not new_ads:
+            message="**Aucune nouvelle annonce !**"
+            channel = client.get_channel(1052934887100915763)
+            await channel.send(message)
+        else:
+            message = ""
+            for title, url in new_ads:
+                message += f"{title} ({url})\n"
+                sql_additem("serveur", title, url)
+
+            await send_message(client, message)
+
+        await client.close()
+
+    client.run(DISCORD_TOKEN)
+
+if __name__ == "__main__":
+    sys.exit(main())
